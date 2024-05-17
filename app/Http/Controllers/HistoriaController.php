6ba7f927d31b037ae8349626798a3a5b1cd3cf0a
Brian Caldera <brian_alex07@outlook.com>
2024-05-16 22:07:14 -0400

    Added Historia model, migration, factory, etc

---
+++ app/Http/Controllers/HistoriaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHistoriaRequest;
+use App\Http\Requests\UpdateHistoriaRequest;
+use App\Models\Historia;
+
+class HistoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHistoriaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Historia $historia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Historia $historia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHistoriaRequest $request, Historia $historia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Historia $historia)
+    {
+        //
+    }
+}
