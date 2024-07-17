9333c247ef230e0af2c8436c91034c6e53a3368a
Brian Caldera <briancaldera07@gmail.com>
2024-07-16 21:12:00 -0400

    Added leading to input label

--- resources/js/Components/atoms/Label.jsx
+++ resources/js/Components/atoms/Label.jsx
@@ -1,6 +1,6 @@
 const Label = ({ value, className = '', children, ...props }) => {
     return (
-        <label {...props} className={`block font-medium text-sm text-slate-700 dark:text-slate-300 ${className}`}>
+        <label {...props} className={`block leading-6 font-medium text-sm text-slate-700 dark:text-slate-300 ${className}`}>
             {value ? value : children}
         </label>
     )
