ddcfef1964339d1f2b11a4b197a1b4f12214f2b9
Brian Caldera <briancaldera07@gmail.com>
2024-07-19 23:11:28 -0400

    Using Vite static asset import

--- resources/js/Components/atoms/Label.jsx
+++ resources/js/Components/atoms/Label.jsx
@@ -1,6 +1,6 @@
 const Label = ({ value, className = '', children, ...props }) => {
     return (
-        <label {...props} className={`block leading-6 font-medium text-sm text-slate-700 dark:text-slate-300 ${className}`}>
+        <label {...props} className={`block leading-6 font-medium text-sm text-gray-700 dark:text-gray-300 ${className}`}>
             {value ? value : children}
         </label>
     )
--- resources/js/Components/atoms/Logo.jsx
+++ resources/js/Components/atoms/Logo.jsx
@@ -1,7 +1,9 @@
+import logoUrl from "/public/UGMA-logo.png"
+
 const Logo = ({className = '', props}) => {
     return (
     <div className={`grid grid-cols-1 min-h-[48px] w-full ${className}`}>
-        <img width={"300"} className={`object-cover object-center aspect-square`} src="/public/UGMA-logo.png" alt="Logo UGMA" {...props}/>
+        <img width={"300"} className={`object-cover object-center aspect-square`} src={logoUrl} alt="Logo UGMA" {...props}/>
     </div>
     )
 }
