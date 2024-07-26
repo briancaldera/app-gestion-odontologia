0063ee037b61b9733c1b5f840efa4a3b47cbda2d
Brian Caldera <briancaldera07@gmail.com>
2024-07-26 16:43:06 -0400

    Base layout assigned now from InertiaApp resolver

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,16 +1,20 @@
 import '@fontsource-variable/inter';
 import './bootstrap';
 import '../css/app.css';
-
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import BaseLayout from "@/Layouts/BaseLayout.jsx";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: async (name) => {
+        const page = await resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx'))
+        page.default.layout = page.default.layout || (page => <BaseLayout children={page}/>)
+        return page
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
