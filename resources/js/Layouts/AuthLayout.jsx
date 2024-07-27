13f63a3b9dd2801e96cb2262079c725a63df238c
Brian Caldera <briancaldera07@gmail.com>
2024-07-27 04:13:54 -0400

    Added Loader to sidebar

--- resources/js/Layouts/AuthLayout.jsx
+++ resources/js/Layouts/AuthLayout.jsx
@@ -1,37 +1,40 @@
-import {Head, usePage} from "@inertiajs/react";
+import {Head, router, usePage} from "@inertiajs/react";
 import React from "react";
-import Sidebar from "@/Components/organisms/Sidebar.jsx";
-import {DocumentIcon} from '@heroicons/react/24/outline'
-import {Icon} from "@/Components/atoms/Icon.jsx";
+import AuthNavbar from "@/Components/organisms/AuthNavbar.jsx";
+import AuthSidebar from "@/Components/organisms/AuthSidebar.jsx"
+import Loader from "@/Components/atoms/Loader.jsx";
 
 const AuthLayout = ({title, navbar, sidebar, children}) => {
 
+    const [loading, setLoading] = React.useState(false)
+
     const {auth} = usePage().props
 
-    const defaultSidebar = (
-        <div className={'grid grid-cols-2 gap-2 p-6 justify-items-center'}>
-            <div className={'aspect-square border-2 rounded-3xl border-dashed border-gray-300 p-10'}>
-                <Icon><DocumentIcon/></Icon>
-            </div>
-            <div className={'aspect-square border-2 rounded-3xl border-dashed border-gray-300 p-10'}>
-                <Icon><DocumentIcon/></Icon>
-            </div>
-            <div className={'aspect-square border-2 rounded-3xl border-dashed border-gray-300 p-10'}>
-                <Icon><DocumentIcon/></Icon>
-            </div>
-            <div className={'aspect-square border-2 rounded-3xl border-dashed border-gray-300 p-10'}>
-                <Icon><DocumentIcon/></Icon>
-            </div>
+    React.useEffect(() => {
+        let timeout = null
+        const onStartListener = router.on('start', () => {
+            timeout = setTimeout(() => setLoading(true), 250)
+        })
+        const onFinishListener = router.on('finish', () => {
+            clearTimeout(timeout)
+            setLoading(false)
+        })
 
-        </div>
-    )
+        return () => {
+            onStartListener()
+            onFinishListener()
+        }
+    }, []);
 
     return (
         <>
             <Head title={title}/>
-            <nav className={'z-100 fixed inset-x-0 top-0 bg-white h-20'}>{navbar}</nav>
+            <nav className={'z-100 fixed inset-x-0 top-0 bg-white h-20'}>{navbar || <AuthNavbar auth={auth}/>}</nav>
             <aside className={'z-90 bg-white fixed inset-y-0 left-0 w-full max-w-72 pt-20'}>
-                {defaultSidebar}
+                {sidebar || <AuthSidebar />}
+                <div className={'z-100 absolute bottom-0 left-0 p-4'} hidden={!loading}>
+                    <Loader />
+                </div>
             </aside>
             <main className={'pt-20 pl-72 min-h-screen'}>
                 {children}
