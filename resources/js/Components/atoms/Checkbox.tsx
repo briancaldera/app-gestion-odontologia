b0d6cfb4d58ce8a8c7f43500e03ef8098fa0c510
Brian Caldera <briancaldera07@gmail.com>
2024-08-07 23:51:43 -0400

    Add Checkbox

---
+++ resources/js/Components/atoms/Checkbox.tsx
@@ -0,0 +1,10 @@
+import {Checkbox as ShadcnCheckbox} from "@/shadcn/ui/checkbox";
+import React from "react";
+
+const Checkbox = ({...props}) => {
+    return (
+        <ShadcnCheckbox {...props} />
+    )
+}
+
+export default Checkbox
---
+++ stories/atoms/Checkbox.stories.tsx
@@ -0,0 +1,10 @@
+import Checkbox from "@/Components/atoms/Checkbox";
+
+export default  {
+    title: 'atoms/Checkbox',
+    component: Checkbox,
+}
+
+export const Default = {
+
+}
