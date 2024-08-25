9896cb6fcea4324966786a560cd000b54fe6ef90
Brian Caldera <briancaldera07@gmail.com>
2024-08-25 03:26:24 -0400

    FIX: Readd hasRole method so Middleware can work

--- app/HasRole.php
+++ app/HasRole.php
@@ -25,4 +25,30 @@ trait HasRole
     {
         return $this->getAttribute($this->role_key) == 3;
     }
+
+    public function hasRole(string $role): bool {
+        return $this->role == $this->mapRoleToRoleId($role);
+    }
+
+    private function mapRoleToRoleId(string $role): int
+    {
+        $id = -1;
+        switch ($role) {
+            case 'admin':
+                $id = 0;
+                break;
+            case 'admision':
+                $id = 1;
+                break;
+            case 'profesor':
+                $id = 2;
+                break;
+            case 'estudiante':
+                $id = 3;
+                break;
+            default:
+                abort('500', 'No such role');
+        }
+        return $id;
+    }
 }
