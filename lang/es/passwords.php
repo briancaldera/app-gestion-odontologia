7f1c885bf73cb83ad91bfed97d2520a60da056e3
Brian Caldera <briancaldera07@gmail.com>
2024-08-27 21:10:16 -0400

    FEAT(lang): Translate auth, pagination and passwords words

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset'    => '¡Tu contraseña ha sido restablecida!',
+    'sent'     => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!📨',
+    'throttled' => 'Por favor, espera antes de reintentar.',
+    'token'    => 'El token de recuperación de contraseña es inválido.',
+    'user'     => 'No podemos encontrar ningún usuario con ese correo electrónico.',
+
+];
