8326e5403eb1cf5b2ee001996ea6253d1898b95e
Brian Caldera <briancaldera07@gmail.com>
2024-08-29 04:17:40 -0400

    FEAT: Add SidebarMenu component

---
+++ resources/js/Components/organisms/SidebarMenu.tsx
@@ -0,0 +1,35 @@
+import {type MenuItem} from '@/src/SidebarMenu'
+import {useRoute} from "ziggy-js";
+import {Link, usePage} from "@inertiajs/react";
+import {Icon} from "@/Components/atoms/Icon";
+import Title from "@/Components/atoms/Title";
+import React from "react";
+
+interface SidebarProps {
+    readonly menu: MenuItem[]
+}
+
+const SidebarMenu = ({menu}: SidebarProps) => {
+
+    const { url } = usePage()
+    const route = useRoute()
+
+    return (
+        <div className={'h-full lg:p-6 flex lg:flex-col gap-1 justify-center lg:justify-start'}>
+            {menu.map((menuItem, index) =>
+                <Link
+                    className={`flex-initial flex items-center gap-2 rounded-lg p-2 cursor-pointer hover:bg-white/10 ${(route(menuItem.link).endsWith(url)) ? 'bg-white/10' : ''}`}
+                    href={route(menuItem.link)} key={index}
+                    onClick={() => {
+                    }}>
+                    <Icon className={'size-8 text-white'}>
+                        {menuItem.icon}
+                    </Icon>
+                    <Title className={'max-lg:hidden text-white'} level={'title-md'}>{menuItem.name}</Title>
+                </Link>
+            )}
+        </div>
+    )
+}
+
+export default SidebarMenu
---
+++ resources/js/src/SidebarMenu.ts
@@ -0,0 +1,7 @@
+import React from "react";
+
+export type MenuItem = Readonly<{
+    name: string,
+    icon: React.ReactElement,
+    link: string
+}>
