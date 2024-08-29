6d10be6c1b7c8bf04a33d360dfd66561309d2144
Brian Caldera <briancaldera07@gmail.com>
2024-08-29 04:17:01 -0400

    FIX: Tidy up validation rules

--- app/Http/Requests/StorePacienteRequest.php
+++ app/Http/Requests/StorePacienteRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Paciente;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\File;
 
@@ -24,17 +25,19 @@ class StorePacienteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'cedula' => ['required', 'string', 'alpha_num:ascii', 'between:4, 10','regex:/^[VE][\d]{3,9}$/', 'unique:App\Models\Paciente,cedula'],
-            'nombre' => ['required', 'string', 'alpha', 'between:2, 50'],
-            'apellido' => ['required', 'string', 'alpha', 'between:2, 50'],
+            'cedula' => ['required', 'string', 'alpha_num:ascii', 'between:4, 10','regex:/^[VE][\d]{3,9}$/', 'unique:'.Paciente::class.',cedula'],
+            'nombre' => ['required', 'string', 'between:2, 50'],
+            'apellido' => ['required', 'string', 'between:2, 50'],
             'edad' => ['required', 'numeric', 'integer', 'min:0', 'max:150'],
-            'sexo' => ['required', 'string', 'alpha:ascii', 'size:1', 'regex:/^[M|F]$/'],
-            'peso' => ['required', 'numeric', 'min:0', 'max:300','decimal:2'],
+            'sexo' => ['required', 'string', 'between:1,2', 'regex:/[M|F|NI]/'],
+            'peso' => ['required', 'numeric', 'min:0', 'max:300','decimal:0,2'],
             'fecha_nacimiento' => ['required', 'date', 'before:today'],
-            'ocupacion' => ['required', 'string', 'alpha', 'between:0, 50'],
+            'ocupacion' => ['required', 'string', 'between:0, 50'],
             'direccion' => ['string', 'between:3, 100'],
             'telefono' => ['nullable', 'string', 'between:0, 15', 'regex:/^[\d]{4}-[\d]{7}$/'],
             'foto' => ['sometimes', 'nullable', 'image', 'dimensions:min_width=100,min_height=100,max_width=2000,max_height=2000', 'min:50', 'max:2000'],
+//            'motivo_consulta' => ['required', 'string', 'max:1000'],
+//            'enfermedad_actual' => ['required', 'string', 'max:1000'],
         ];
     }
 }
--- app/Http/Requests/UpdatePacienteRequest.php
+++ app/Http/Requests/UpdatePacienteRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Paciente;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdatePacienteRequest extends FormRequest
@@ -22,14 +23,14 @@ class UpdatePacienteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'cedula' => ['sometimes', 'required', 'string', 'alpha_num:ascii', 'between:4, 10','regex:/^[VE][\d]{3,9}$/', 'unique:App\Models\Paciente,cedula'],
+            'cedula' => ['sometimes', 'required', 'string', 'alpha_num:ascii', 'between:4, 10','regex:/^[VE][\d]{3,9}$/', 'exists:'.Paciente::class.',cedula'],
             'nombre' => ['sometimes', 'required', 'string', 'between:2, 50'],
             'apellido' => ['sometimes', 'required', 'string', 'between:2, 50'],
             'edad' => ['sometimes', 'required', 'numeric', 'integer', 'min:0', 'max:150'],
-            'sexo' => ['sometimes', 'required', 'string', 'alpha:ascii', 'size:1', 'regex:/^[M|F]$/'],
-            'peso' => ['sometimes', 'required', 'numeric', 'min:0', 'max:300','decimal:2'],
+            'sexo' => ['sometimes', 'required', 'string', 'between:1,2', 'regex:/[M|F|NI]/'],
+            'peso' => ['sometimes', 'required', 'numeric', 'min:0', 'max:300','decimal:0,2'],
             'fecha_nacimiento' => ['sometimes', 'required', 'date', 'before:today'],
-            'ocupacion' => ['sometimes', 'required', 'string', 'alpha', 'between:0, 50'],
+            'ocupacion' => ['sometimes', 'required', 'string', 'between:0, 50'],
             'direccion' => ['sometimes', 'string', 'between:3, 100'],
             'telefono' => ['sometimes', 'nullable', 'string', 'between:0, 15', 'regex:/^[\d]{4}-[\d]{7}$/'],
             'foto' => ['sometimes', 'nullable', 'image', 'dimensions:min_width=100,min_height=100,max_width=2000,max_height=2000', 'min:50', 'max:2000'],
