e2209c8a8fd001a2b2c6dbe44dc1a578d36980d2
Brian Caldera <briancaldera07@gmail.com>
2024-09-06 04:17:20 -0400

    Add better method to highlight current route

--- resources/js/Components/organisms/SidebarMenu.tsx
+++ resources/js/Components/organisms/SidebarMenu.tsx
@@ -1,24 +1,22 @@
-import {type MenuItem} from '@/src/SidebarMenu'
 import {useRoute} from "ziggy-js";
-import {Link, usePage} from "@inertiajs/react";
+import {Link} from "@inertiajs/react";
 import {Icon} from "@/Components/atoms/Icon";
 import Title from "@/Components/atoms/Title";
 import React from "react";
 
-interface SidebarProps {
+type SidebarProps = {
     readonly menu: MenuItem[]
 }
 
 const SidebarMenu = ({menu}: SidebarProps) => {
 
-    const { url } = usePage()
     const route = useRoute()
 
     return (
         <div className={'h-full lg:p-6 flex lg:flex-col gap-1 justify-center lg:justify-start'}>
             {menu.map((menuItem, index) =>
                 <Link
-                    className={`flex-initial flex items-center gap-2 rounded-lg p-2 cursor-pointer hover:bg-white/10 ${(route(menuItem.link).endsWith(url)) ? 'bg-white/10' : ''}`}
+                    className={`flex-initial flex items-center gap-2 rounded-lg p-2 cursor-pointer hover:bg-white/10 ${(route().current() === menuItem.link) ? 'bg-white/10' : ''}`}
                     href={route(menuItem.link)} key={index}
                     onClick={() => {
                     }}>
