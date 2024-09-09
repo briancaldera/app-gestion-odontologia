7f20d1e7c774dc6a90d8622deed6123a5dd897c1
Brian Caldera <briancaldera07@gmail.com>
2024-09-09 02:22:56 -0400

    Add type to HistoriaOdontologica

--- resources/js/src/models/HistoriaOdontologica.ts
+++ resources/js/src/models/HistoriaOdontologica.ts
@@ -1,13 +1,76 @@
 import {z} from 'zod'
-import HistoriaOdontologicaSchema from "@/FormSchema/Historia/HistoriaOdontologicaSchema";
-import PlanTratamientoSchema from "@/FormSchema/Historia/PlanTratamientoSchema";
 import EstudioModelosSchema from "@/FormSchema/Historia/EstudioModelosSchema";
+import ExamenRadiograficoSchema from "@/FormSchema/Historia/ExamenRadiograficoSchema.ts";
 
-type HistoriaOdontologica = z.infer<typeof HistoriaOdontologicaSchema> & {
+type HistoriaOdontologica = {
+    historia_id: string
+    ant_personales: string
+    habitos: string
+    portador: Portador
+    examen_fisico: ExamenFisico
     estudio_modelos: z.infer<typeof EstudioModelosSchema>
-    plan_tratamiento: any
-    modificaciones_plan_tratamiento: any
-    secuencia_tratamiento: any
+    plan_tratamiento: Tratamiento[]
+    modificaciones_plan_tratamiento: ModificacionTratamiento[]
+    secuencia_tratamiento: TratamientoRealizado[]
+    examen_radiografico: z.infer<typeof ExamenRadiograficoSchema>
 }
+
+type Portador = {
+    ortodoncia: boolean
+    protesis: boolean
+}
+
+type Tratamiento = {
+    cavidad: string
+    diente: string
+    tratamiento: string
+}
+
+type ModificacionTratamiento = {
+    diente: string
+    fecha: string
+    tratamiento: string
+}
+
+type TratamientoRealizado = {
+    diente: string
+    fecha: string
+    tratamiento: string
+}
+
+const examen_fisico = {
+    examen_extraoral: {
+        articulacion_temporomandibular: "",
+        cabeza: "",
+        cara: "",
+        lesiones_extraorales: "",
+        palpacion_ganglios: "",
+        piel: "",
+        simetria_facial: ""
+    },
+    examen_intraoral: {
+        dientes: "",
+        discromias: "",
+        encias: "",
+        frenillos: "",
+        labios: "",
+        lengua_tipo: "",
+        maxilares: "",
+        mejillas: "",
+        paladar_duro_blando: "",
+        piso_boca: ""
+    },
+    signos_vitales: {
+        pulso: 0,
+        respiracion: 0,
+        temperatura: 0,
+        tension_arterial: {
+            diastole: 0,
+            sistole: 0
+        }
+    }
+}
+
+type ExamenFisico = typeof examen_fisico
 
 export default HistoriaOdontologica
