1f581969287461b7615df7c23899218269ee15c3
Brian Caldera <briancaldera07@gmail.com>
2024-09-20 14:54:34 -0400

    Add Paciente views

---
+++ resources/js/Pages/Estudiante/Pacientes/Index.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "@/Layouts/AuthLayout.tsx";
+
+const Index = () => {
+    return (
+        <AuthLayout title={'Pacientes'}>
+
+        </AuthLayout>
+    )
+}
+
+export default Index
---
+++ resources/js/Pages/Estudiante/Pacientes/Show.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "@/Layouts/AuthLayout.tsx";
+
+const Show = () => {
+    return (
+        <AuthLayout title={'Paciente - '}>
+
+        </AuthLayout>
+    )
+}
+
+export default Show
