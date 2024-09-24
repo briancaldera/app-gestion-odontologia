b25603b07c52482c1f86f75cdfd396a5879a066d
Brian Caldera <briancaldera07@gmail.com>
2024-09-23 21:04:02 -0400

    Add hook for online status and permissions

--- resources/js/Pages/Estudiante/Pacientes/Show.tsx
+++ resources/js/Pages/Estudiante/Pacientes/Show.tsx
@@ -1,9 +1,29 @@
 import AuthLayout from "@/Layouts/AuthLayout.tsx";
+import Paciente from "@/src/models/Paciente.ts";
+import React from "react";
+import {usePermission} from "@/src/Utils/Utils.ts";
+import Surface from "@/Components/atoms/Surface";
 
-const Show = () => {
+type ShowProps = {
+    paciente: Paciente
+}
+
+const Show = ({paciente}: ShowProps) => {
+
+    const can = usePermission()
+
     return (
-        <AuthLayout title={'Paciente - '}>
+        <AuthLayout title={`Paciente - ${paciente.nombre} ${paciente.apellido}`}>
+            <div className={'h-full p-6 grid grid-cols-3 gap-6'}>
+                <Surface className={'col-span-2'}>
 
+
+                </Surface>
+                <Surface className={'col-span-1'}>
+
+
+                </Surface>
+            </div>
         </AuthLayout>
     )
 }
--- resources/js/src/Utils/Utils.ts
+++ resources/js/src/Utils/Utils.ts
@@ -1,6 +1,8 @@
 import {UseFormReturn} from "react-hook-form";
+import {usePage} from "@inertiajs/react";
+import React from "react";
 
-export const mapServerErrorsToFields = function (form: UseFormReturn, errors: Record<string, string>) {
+const mapServerErrorsToFields = function (form: UseFormReturn, errors: Record<string, string>) {
     Object.keys(errors).forEach(key => form.setError(key, {type: 'custom', message: errors[key]}))
 }
 
@@ -9,7 +11,7 @@ export const mapServerErrorsToFields = function (form: UseFormReturn, errors: Re
  * @param item
  * @returns {boolean}
  */
-export function isObject(item) {
+function isObject(item) {
     return (item && typeof item === 'object' && !Array.isArray(item));
 }
 
@@ -18,7 +20,7 @@ export function isObject(item) {
  * @param target
  * @param ...sources
  */
-export function mergeDeep(target, ...sources) {
+function mergeDeep(target, ...sources) {
     if (!sources.length) return target;
     const source = sources.shift();
 
@@ -35,3 +37,35 @@ export function mergeDeep(target, ...sources) {
 
     return mergeDeep(target, ...sources);
 }
+
+const usePermission = (): ((permission: string) => boolean) => {
+    const {auth: {user}} = usePage().props
+
+    const can = React.useCallback((permission: string): boolean => (user?.permissions ?? []).some(p => p === permission), [user])
+    return can
+}
+
+/**
+* Stolen from https://react.dev/learn/reusing-logic-with-custom-hooks
+ * 😎
+*/
+const useOnlineStatus = () => {
+    const [isOnline, setIsOnline] = React.useState(true);
+    React.useEffect(() => {
+        function handleOnline() {
+            setIsOnline(true);
+        }
+        function handleOffline() {
+            setIsOnline(false);
+        }
+        window.addEventListener('online', handleOnline);
+        window.addEventListener('offline', handleOffline);
+        return () => {
+            window.removeEventListener('online', handleOnline);
+            window.removeEventListener('offline', handleOffline);
+        };
+    }, []);
+    return isOnline;
+}
+
+export {usePermission, useOnlineStatus, mergeDeep, isObject, mapServerErrorsToFields}
