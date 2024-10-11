e27445e5721439fc56d081a92fbeaeba6c63583a
Brian Caldera <briancaldera07@gmail.com>
2024-10-11 14:44:52 -0400

    Strict mode

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import BaseLayout from "@/Layouts/BaseLayout.tsx";
+import { StrictMode } from 'react';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,7 +19,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <StrictMode>
+                <App {...props} />
+            </StrictMode>
+    );
     },
     progress: {
         color: '#4B5563',
