52495b7ed03754a25722e19bbf185ef598dd6a71
Brian Caldera <briancaldera07@gmail.com>
2024-10-21 17:19:34 -0400

    Setup emails content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verificación de correo electrónico')
+                ->greeting('Hola! 🦷')
+                ->line('Por favor, presiona el botón debajo para verificar tu correo electrónico.')
+                ->action('Verificar 👍🏻', $url)
+                ->salutation("Nuestros mejores deseos en tus estudios. " . env('APP_NAME', 'UGMA - Facultad de Odontología'));
+        });
+
+        ResetPassword::toMailUsing(
+            function (object $notifiable, string $token) {
+                $url = route('password.reset', ['token' => $token]);
+
+                return (new MailMessage)
+                    ->subject('Restablecer contraseña')
+                    ->greeting('Hola! 🦷')
+                    ->line('Al parecer, has olvidado tu contraseña de acceso al sistema. Por favor, presiona el botón debajo para restablecer tu contraseña.')
+                    ->action('Restablecer contraseña', $url)
+                    ->salutation("Saludos. " . env('APP_NAME', 'UGMA - Facultad de Odontología'));
+            }
+        );
     }
 }
