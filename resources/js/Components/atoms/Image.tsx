7402c4d9e79216bfbd1b3b8521258e193af2c098
Brian Caldera <briancaldera07@gmail.com>
2024-10-26 22:02:32 -0400

    Fix

--- resources/js/Components/atoms/Image.tsx
+++ resources/js/Components/atoms/Image.tsx
@@ -1,30 +1,26 @@
 import React, {ClassAttributes} from "react";
+import {cn} from "@/lib/utils.ts";
 
 type ImageProps = {
     src: string | File | null
+    className?: string
 }
 
-const Image = ({
-                   src,
-                   ...props
-               }: ImageProps & ClassAttributes<HTMLImageElement> & React.ImgHTMLAttributes<HTMLImageElement>) => {
+const Image = ({src, className, ...props}: ImageProps & ClassAttributes<HTMLImageElement> & React.ImgHTMLAttributes<HTMLImageElement>) => {
 
     const preview: string | undefined = React.useMemo(() => {
-        if (src instanceof File) {
-            return URL.createObjectURL(src)
-        } else if (typeof src === 'string') {
-            return src
-        }
-    }, [])
 
-    React.useEffect(() => {
-        return () => {
-            // if (src instanceof File) URL.revokeObjectURL(preview)
+        if (!src) return
+
+        if (typeof src === 'string') {
+            return src
+        } else if (src instanceof File) {
+            return URL.createObjectURL(src)
         }
     }, [])
 
     return (
-        <img src={preview} alt={''} {...props}/>
+        <img src={preview} alt={''} className={cn('object-contain', className)} {...props}/>
     )
 }
 
