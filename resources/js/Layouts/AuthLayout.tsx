a12b311c86c956673ab7ab0e30bd85bd990af02b
Brian Caldera <briancaldera07@gmail.com>
2024-11-17 23:19:28 -0400

    🛠️ Fix SidebarMenu and layout height in mobile

--- resources/js/Layouts/AuthLayout.tsx
+++ resources/js/Layouts/AuthLayout.tsx
@@ -1,9 +1,7 @@
-import {Head, Link, router, usePage} from "@inertiajs/react";
+import {Head, router} from "@inertiajs/react";
 import React from "react";
 import AuthNavbar from "@/Components/organisms/AuthNavbar.tsx";
 import Loader from "@/Components/atoms/Loader.tsx";
-import Logo from "@/Components/atoms/Logo.js";
-import {useRoute} from 'ziggy-js'
 import {TooltipProvider} from "@/Components/atoms/Tooltip"
 import SidebarMenu from "@/Components/organisms/SidebarMenu.tsx";
 
@@ -13,8 +11,6 @@ export const AuthContext = React.createContext({})
 
 const AuthLayout = ({title, sidebar, children}) => {
 
-    const {auth: {user}} = usePage().props
-    const route = useRoute()
     const [loading, setLoading] = React.useState(false)
     const [isDarkMode, toggleDarkMode] = React.useState(false)
 
@@ -52,22 +48,20 @@ const AuthLayout = ({title, sidebar, children}) => {
         })
     }
 
-    const can = React.useCallback((permission: string): boolean => (user?.permissions ?? []).some(p => p === permission), [user]) // todo remove this
-
     const [isSidebarExpanded, setIsSidebarExpanded] = React.useState<boolean>(true)
 
     return (
-        <AuthContext.Provider value={{isDarkMode: isDarkMode, toggleDarkMode: handleToggleDarkMode, can: can}}>
+        <AuthContext.Provider value={{isDarkMode: isDarkMode, toggleDarkMode: handleToggleDarkMode}}>
             <TooltipProvider>
                 <Head title={title}/>
-                <div className={`${isDarkMode ? 'dark' : ''} bg-slate-100 dark:bg-slate-900 h-screen flex flex-col`}>
+                <div className={`${isDarkMode ? 'dark' : ''} bg-slate-100 dark:bg-slate-900 h-dvh flex flex-col`}>
                     <SidebarMenu expanded={isSidebarExpanded}/>
                     <AuthNavbar/>
                     <div className={'z-50 absolute bottom-0 left-0 p-4'} hidden={!loading}>
                         <Loader/>
                     </div>
                     <main
-                        className={`pt-14 sm:pt-20 lg:pb-0 pb-16 ${isSidebarExpanded ? 'lg:pl-72' : 'lg:pl-28'} h-full basis-full flex-none`}>
+                        className={`pt-14 sm:pt-20 lg:pb-0 pb-10 ${isSidebarExpanded ? 'lg:pl-72' : 'lg:pl-28'} h-full basis-full flex-none`}>
                         {children}
                     </main>
                 </div>
