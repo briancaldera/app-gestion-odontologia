da01e641de61b4e985d50861568aa910dffc378c
Brian Caldera <briancaldera07@gmail.com>
2024-11-27 03:14:34 -0400

    ⚙️ Setup API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -26,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->trustProxies(at: '*');
         $middleware->trustHosts(at: ['ugma-app-gestion-odontologia-96559078110.us-east1.run.app']);
+        $middleware->statefulApi();
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
