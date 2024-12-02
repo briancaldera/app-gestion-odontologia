4b56ae50a5eaac8711a1a18b182faec597f1e925
Brian Caldera <briancaldera07@gmail.com>
2024-12-02 05:34:42 -0400

    🔧 Fix fullscreen

--- resources/js/Components/organisms/historia/partials/InterpretacionCoronales.tsx
+++ resources/js/Components/organisms/historia/partials/InterpretacionCoronales.tsx
@@ -13,6 +13,7 @@ import Image from "@/Components/atoms/Image.tsx";
 import DragAndDrop from "@/Components/molecules/DragAndDrop";
 import {Textarea} from "@/shadcn/ui/textarea.tsx";
 import {Button} from "@/shadcn/ui/button.tsx";
+import {Maximize2} from "lucide-react";
 
 const ACCEPTED_PICTURE_MIME: readonly string[] = ['image/jpeg', 'image/jpg', 'image/png']
 
@@ -86,21 +87,27 @@ const InterpretacionCoronales = () => {
 
                         <div className={'px-14'}>
                             <Carousel>
-                                <CarouselContent className={'h-[700px]'}>
+                                <CarouselContent className={'h-[700px] bg-black'}>
                                     {
-                                        historia_odontologica?.coronales.map((url: string) => (
-                                            <CarouselItem key={url} className={'bg-white'}>
+                                        historia_odontologica?.coronales.map((url: string, index) => (
+                                            <CarouselItem key={url} className={'bg-black relative'}>
+                                                <Button type='button' className={'absolute top-0 right-0'} variant='ghost' onClick={() => document.querySelector(`#coro_image_server_${index}`)?.requestFullscreen()}>
+                                                    <Maximize2 className={'mr-2 text-white'}/>
+                                                </Button>
                                                 <div className={'h-full flex justify-center items-center'}>
-                                                    <Image src={url} className={'object-contain h-full w-full'}/>
+                                                    <Image src={url} className={'object-contain h-full w-full'} id={`coro_image_server_${index}`}/>
                                                 </div>
                                             </CarouselItem>
                                         ))
                                     }
                                     {
-                                        interpretacionCoronalesForm.getValues().interpretacion_coronales.imagenes.map((file: File) => (
-                                            <CarouselItem key={file.name} className={'bg-white'}>
+                                        interpretacionCoronalesForm.getValues().interpretacion_coronales.imagenes.map((file: File, index) => (
+                                            <CarouselItem key={file.name} className={'bg-black relative'}>
+                                                <Button type='button' className={'absolute top-0 right-0'} variant='ghost' onClick={() => document.querySelector(`#coro_image_local_${index}`)?.requestFullscreen()}>
+                                                    <Maximize2 className={'mr-2 text-white'}/>
+                                                </Button>
                                                 <div className={'h-full flex justify-center items-center'}>
-                                                    <Image src={file} className={'object-contain h-full w-full'}/>
+                                                    <Image src={file} className={'object-contain h-full w-full'} id={`coro_image_local_${index}`}/>
                                                 </div>
                                             </CarouselItem>
                                         ))
--- resources/js/Components/organisms/historia/partials/InterpretacionPeriapicales.tsx
+++ resources/js/Components/organisms/historia/partials/InterpretacionPeriapicales.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {HistoriaEditorContext} from "@/Components/organisms/HistoriaEditor.tsx";
 import useInertiaSubmit from "@/src/inertia-wrapper/InertiaSubmit.ts";
 import {useForm} from "react-hook-form";
-import {undefined, z} from "zod";
+import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {examenRadiograficoSchema} from "@/FormSchema/Historia/ExamenRadiograficoSchema.ts";
 import {mapServerErrorsToFields} from "@/src/Utils/Utils.ts";
@@ -13,6 +13,7 @@ import Image from "@/Components/atoms/Image.tsx";
 import DragAndDrop from "@/Components/molecules/DragAndDrop";
 import {Textarea} from "@/shadcn/ui/textarea.tsx";
 import {Button} from "@/shadcn/ui/button.tsx";
+import {Maximize2} from "lucide-react";
 
 const ACCEPTED_PICTURE_MIME: readonly string[] = ['image/jpeg', 'image/jpg', 'image/png']
 
@@ -85,21 +86,27 @@ const InterpretacionPeriapicales = () => {
 
                         <div className={'px-14'}>
                             <Carousel>
-                                <CarouselContent className={'h-[700px]'}>
+                                <CarouselContent className={'h-[700px] bg-black'}>
                                     {
-                                        historia_odontologica?.periapicales.map((url: string) => (
-                                            <CarouselItem key={url} className={'bg-white'}>
+                                        historia_odontologica?.periapicales.map((url: string, index) => (
+                                            <CarouselItem key={url} className={'bg-black relative'}>
+                                                <Button type='button' className={'absolute top-0 right-0'} variant='ghost' onClick={() => document.querySelector(`#peri_image_server_${index}`)?.requestFullscreen()}>
+                                                    <Maximize2 className={'mr-2 text-white'}/>
+                                                </Button>
                                                 <div className={'h-full flex justify-center items-center'}>
-                                                    <Image src={url} className={'object-contain h-full w-full'}/>
+                                                    <Image src={url} className={'object-contain h-full w-full'} id={`peri_image_server_${index}`}/>
                                                 </div>
                                             </CarouselItem>
                                         ))
                                     }
                                     {
-                                        interpretacionPeriapicalesForm.getValues().interpretacion_periapicales.imagenes.map((file: File) => (
-                                            <CarouselItem key={file.name} className={'bg-white'}>
+                                        interpretacionPeriapicalesForm.getValues().interpretacion_periapicales.imagenes.map((file: File, index) => (
+                                            <CarouselItem key={file.name} className={'bg-black relative'}>
+                                                <Button type='button' className={'absolute top-0 right-0'} variant='ghost' onClick={() => document.querySelector(`#peri_image_local_${index}`)?.requestFullscreen()}>
+                                                    <Maximize2 className={'mr-2 text-white'}/>
+                                                </Button>
                                                 <div className={'h-full flex justify-center items-center'}>
-                                                    <Image src={file} className={'object-contain h-full w-full'}/>
+                                                    <Image src={file} className={'object-contain h-full w-full'} id={`peri_image_local_${index}`}/>
                                                 </div>
                                             </CarouselItem>
                                         ))
