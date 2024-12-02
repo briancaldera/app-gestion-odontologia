6c2a8aa5c0fee5a77045a7f9a0b42bfb0a3a83ec
Brian Caldera <briancaldera07@gmail.com>
2024-12-02 06:57:45 -0400

    🔧 Create quietly

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Endodoncia\HistoriaEndodoncia;
-use App\Models\Group;
-use App\Models\Group\Assignment;
-use App\Models\Group\Homework;
-use App\Models\Historia;
 use App\Models\Paciente;
-use App\Models\Permission;
 use App\Models\Profile;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Collection;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -26,35 +18,35 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([PermissionsSeeder::class]);
 
-        $admin = User::factory()->has(Profile::factory())->create([
+        $admin = User::factory()->has(Profile::factory())->createQuietly([
             'name' => 'Administracion',
             'email' => 'admin@example.com'
         ]);
         $admin->addRole('admin');
 
-        $admision = User::factory()->has(Profile::factory())->create([
+        $admision = User::factory()->has(Profile::factory())->createQuietly([
             'name' => 'Admision',
             'email' => 'admision@example.com'
         ]);
         $admision->addRole('admision');
 
-        $profesor = User::factory()->has(Profile::factory())->create([
+        $profesor = User::factory()->has(Profile::factory())->createQuietly([
             'name' => 'Profesor',
             'email' => 'profesor@example.com'
         ]);
         $profesor->addRole('profesor');
 
-        $estudianteUser = User::factory()->has(Profile::factory())->create([
+        $estudianteUser = User::factory()->has(Profile::factory())->createQuietly([
             'name' => 'Estudiante',
             'email' => 'estudiante@example.com'
         ]);
         $estudianteUser->addRole('estudiante');
 
-        $admins = User::factory()->has(Profile::factory())->count(3)->createMany()->each(fn(User $user) => $user->addRole('admin'));
-        $admision = User::factory()->has(Profile::factory())->count(5)->createMany()->each(fn(User $user) => $user->addRole('admision'));
-        $profesores = User::factory()->has(Profile::factory())->count(10)->createMany()->each(fn(User $user) => $user->addRole('profesor'));
-        $estudiantes = User::factory()->has(Profile::factory())->count(50)->createMany()->each(fn(User $user) => $user->addRole('estudiante'));
+        $admins = User::factory()->has(Profile::factory())->count(3)->createManyQuietly()->each(fn(User $user) => $user->addRole('admin'));
+        $admision = User::factory()->has(Profile::factory())->count(5)->createManyQuietly()->each(fn(User $user) => $user->addRole('admision'));
+        $profesores = User::factory()->has(Profile::factory())->count(10)->createManyQuietly()->each(fn(User $user) => $user->addRole('profesor'));
+        $estudiantes = User::factory()->has(Profile::factory())->count(50)->createManyQuietly()->each(fn(User $user) => $user->addRole('estudiante'));
 
-        $paciente = Paciente::factory()->for($estudianteUser, 'medicoTratante')->createOne(['registered_by' => $estudianteUser->id]);
+        $paciente = Paciente::factory()->for($estudianteUser, 'medicoTratante')->createOneQuietly(['registered_by' => $estudianteUser->id]);
     }
 }
