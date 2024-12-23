8a07ff302d0b4f8d8d1d747736612e79632bae0b
Brian Caldera <briancaldera07@gmail.com>
2024-12-22 20:32:41 -0400

    🧪 Translate tests name
    -Remove useless tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -11,23 +11,23 @@ beforeEach(function () {
     $this->seed();
 });
 
-test('factory is working', function () {
+test('factory esta funcionando', function () {
     expect(User::factory()->create())->not()->toThrow(Exception::class);
 });
 
-test('has UUID', function () {
+test('usuario tiene UUID', function () {
     $user = User::factory()->create();
 
     expect($user->id)->toBeUuid();
 });
 
-test('has username', function () {
+test('usuario tiene nombre de usuario', function () {
     $user = User::factory()->create();
 
     expect($user->name)->toBeString();
 });
 
-test('has email', function () {
+test('usuario tiene correo electronico', function () {
     $user = User::factory()->create();
 
     expect($user->email)->toBeString()->toContain('@');
@@ -40,23 +40,9 @@ test('has timestamps', function () {
     assertArrayHasKey('updated_at', $user);
 });
 
-test('password is not visible in frontend', function () {
+test('la contrasena no se envia al frontend', function () {
     $user = User::factory()->create();
 
     $serialized = $user->attributesToArray();
     assertArrayNotHasKey('password', $serialized);
-});
-
-test('has role', function () {
-    $user = User::factory()->create();
-
-    assertArrayHasKey('role', $user);
-    expect($user->role)->toBeNumeric()->toBeBetween(0, 3);
-});
-
-test('default role is estudiante', function () {
-    $user = User::factory()->create();
-
-    expect($user->role)->toBe(3);
-    expect($user->isEstudiante())->toBeTrue();
 });
